a8abfa3d6e8844b1b9456a8e35a4f8fb6167c4f8
Haben Kinfu <haben15hkte@gmail.com>
2026-08-28 23:37:56 +0300

    fixed a bug inside bot.py

--- src/bot.py
+++ src/bot.py
@@ -141,7 +141,10 @@ async def convert_command(update, context):
         )
         return
     
-
+    if not img_folder.exists():
+        await update.message.reply_text("You haven't uploaded any image")
+        return
+    
     user_folder = Path('../downloads') / str(user_id)
     
     img_folder = user_folder / 'images'
